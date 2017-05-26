b4545cf6c654198a3679fb3fa11c2e58bc061d53
Oliver Jan Krylow <oliver@bugabinga.net>
2017-05-26 20:25:33 +0200

    Added ammonia and hyper to deps.

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+extern crate hyper;
+
+use hyper::client::Client;
+
+fn main() {
+    //TODO: download a simple article and sanitaize it using the default ammonia
+    // settings.
+    // Start testing with lobste.rs
+    //TODO find simpler request library, do socket shit or find hyper sll thing...
+    let http_client = Client::new();
+
+    let http_download_request = http_client.get("https://www.google.com").send().unwrap();
+
+    println!("The status is: {:?}", http_download_request.status);
+}
