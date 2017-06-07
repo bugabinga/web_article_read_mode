d729e77895ac68c4e9ede4b7dd666f789aa1aad6
Oliver Jan Krylow <oliver@bugabinga.net>
2017-06-07 22:31:48 +0200

    heelo

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     //TODO find simpler request library, do socket shit or find hyper sll thing...
     let http_client = Client::new();
 
-    let http_download_request = http_client.get("https://www.google.com").send().unwrap();
+    let http_download_request = http_client.get("http://www.google.com").send().unwrap();
 
     println!("The status is: {:?}", http_download_request.status);
 }
